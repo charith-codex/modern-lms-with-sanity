1a388c3e78ee284eb2ad9c368a891ca23e961795
Charith Wickramasinghe <pvmcharith@gmail.com>
2025-03-09 23:45:53 +0530

    design hero section

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,3 +1,10 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
-  return <>hello, NextEra</>;
+  return (
+    <div className="min-h-screen bg-background">
+      <Hero />
+      hello, NextEra
+    </div>
+  );
 }
